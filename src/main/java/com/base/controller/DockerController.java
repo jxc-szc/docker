cca52dd1391454f66b3795a9bb655c9b6f7947f3
songzhicheng <song.zhicheng@trs.com.cn>
2018-12-18 16:11:23 +0800

    代码更改测试

--- src/main/java/com/base/controller/DockerController.java
+++ src/main/java/com/base/controller/DockerController.java
@@ -15,11 +15,11 @@ public class DockerController {
         for (int i = 0; i < Integer.parseInt(n); i++) {
             try {
                 log.info("now:" + i);
-                Thread.sleep(2000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        return "docker is fun!";
+        return "docker is so fun!";
     }
 }
