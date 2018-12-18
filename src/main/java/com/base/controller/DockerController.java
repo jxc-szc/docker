f60f9131162c73a6a5cbcadf6cf808e77ea63203
songzhicheng <song.zhicheng@trs.com.cn>
2018-12-18 14:11:37 +0800

    docker init

---
+++ src/main/java/com/base/controller/DockerController.java
@@ -0,0 +1,25 @@
+package com.base.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/docker")
+@Slf4j
+public class DockerController {
+
+    @RequestMapping("say")
+    public String say(String n) {
+        for (int i = 0; i < Integer.parseInt(n); i++) {
+            try {
+                log.info("now:" + i);
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return "docker is fun!";
+    }
+}
